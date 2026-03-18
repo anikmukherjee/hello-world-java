e036f1f2756bb94dd9dbc3c60d6e4332166c8341
anikmukherjee <anikmukherjee@yahoo.com>
2026-03-18 12:30:09 -0400

    Add doNothing method to HelloWorld class

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,11 @@ public class HelloWorld
 { 
   public static void main(String[] args) 
   { 
-    System.out.println("Hello, World! by Anik"); 
+    System.out.println("Hello, World! by Anik");
   } 
+
+  public static void doNothing(String str)
+  {
+    
+  }
 }
