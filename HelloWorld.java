fc1377518aab13896a9d8158588731af3165582b
anikmukherjee <anikmukherjee@yahoo.com>
2026-03-18 12:31:45 -0400

    Add GREETING constant and update doNothing method
    Added a constant GREETING and modified doNothing method to return it.

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,7 @@
 public class HelloWorld 
 { 
+  private static final String GREETING = "Hello, World! by Anik";
+  
   public static void main(String[] args) 
   { 
     System.out.println("Hello, World! by Anik");
@@ -7,6 +9,6 @@ public class HelloWorld
 
   public static void doNothing(String str)
   {
-    
+     return GREETING;
   }
 }
